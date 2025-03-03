e73baf713df5a07e94c2dc85fb7038427b7e5507
fadedfairlyy <fadedfairlyreal@gmail.com>
2025-03-02 19:51:15 -0600

    Update SystemInfo.ps1

--- SystemInfo.ps1
+++ SystemInfo.ps1
@@ -20,12 +20,21 @@ if ($ChassisType -match "8|9|10|11|14|18|21") {
     $DeviceType = "PC"
 }
 
+# Get Storage type and size (SSD or HDD)
+$Disk = Get-CimInstance Win32_DiskDrive | Select-Object Model, MediaType, @{Name="SizeGB";Expression={[math]::round($_.Size / 1GB)}}
+
 # Display information
 Write-Host "CPU: $CPU"
 Write-Host "GPU: $GPU"
 Write-Host "RAM: $RAM GB"
 Write-Host "Screen Resolution: $ResolutionWidth x $ResolutionHeight"
 Write-Host "Device Type: $DeviceType"
+
+# Display Disk Information (SSD/HDD and Size)
+foreach ($d in $Disk) {
+    Write-Host "Disk Type: $($d.MediaType)"
+    Write-Host "Disk Size: $($d.SizeGB) GB"
+}
 
 # Keep the window open
 Read-Host "Press Enter to exit"
